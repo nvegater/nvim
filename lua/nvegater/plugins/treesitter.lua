f2ff2196134c32f25127d6bc3001344d4734afb1
Nicolas Vega Terrazas <nvegater@Mac.fritz.box>
2026-02-13 13:38:42 +0100

    Corrects treesitter config after update

--- lua/nvegater/plugins/treesitter.lua
+++ lua/nvegater/plugins/treesitter.lua
@@ -5,59 +5,46 @@ return {
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
-
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			modules = {},
-			sync_install = true,
-			ignore_install = {},
-			auto_install = true,
-			highlight = {
-				enable = true,
+	opts = {
+		modules = {},
+		sync_install = true,
+		ignore_install = {},
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+		indent = { enable = true },
+		ensure_installed = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"c_sharp",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "∑", -- Alt + w
+				node_incremental = "∑", -- Alt + w
+				scope_incremental = "∑", -- Alt + w
+				node_decremental = "‰", -- Alt + Shift + w
 			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c",
-				"c_sharp",
-			},
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "∑", -- Alt + w
-					node_incremental = "∑", -- Alt + w
-					scope_incremental = "∑", -- Alt + w
-					node_decremental = "‰", -- Alt + Shift + w
-				},
-			},
-		})
-	end,
+		},
+	},
 }
